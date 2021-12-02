3a9af646bf381c98e9c42820431c04798f3ea1fc
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-02 13:28:12 -0800

    componentized AddColorModal

---
+++ AddColorModal.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { Modal, View, Text, Pressable, TextInput } from "react-native";
+import styles from "./styles";
+import ColorPicker from "react-native-wheel-color-picker";
+import { Ionicons } from "react-native-vector-icons";
+
+function AddColorModal({ modalVisible, setModalVisible }) {
+  const [color, setColor] = useState("#ffffff");
+  const [colorName, setColorName] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <Modal
+      style={styles.addColorModal}
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        Alert.alert("Modal has been closed.");
+        setModalVisible(!modalVisible);
+      }}
+    >
+      <View style={styles.centeredView}>
+        <View
+          style={{
+            width: "90%",
+            height: "75%",
+            backgroundColor: color,
+            opacity: 1,
+            borderRadius: 10,
+            display: "flex",
+            flexDirection: "column",
+
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 1 },
+            shadowOpacity: 0.5,
+            shadowRadius: 5,
+          }}
+        >
+          <Text style={styles.colorNameLabel}>Color Name:</Text>
+          <TextInput
+            style={styles.input}
+            value={colorName}
+            onChangeText={(text) => setColorName(text)}
+          />
+          <Text style={styles.descriptionLabel}>Description:</Text>
+          <Text style={styles.descriptionText}>
+            (List of Words Separated By Commas)
+          </Text>
+          <TextInput
+            style={styles.input}
+            value={description}
+            onChangeText={(text) => setDescription(text)}
+          />
+          <Pressable style={styles.submitButton} onPress={submitColor}>
+            <Text>Submit</Text>
+          </Pressable>
+          <Text>{color}</Text>
+          <ColorPicker
+            style={styles.picker}
+            color={color}
+            onColorChange={(c) => setColor(c)}
+            // onColorChangeComplete={this.onColorChangeComplete}
+            thumbSize={30}
+            sliderSize={20}
+            noSnap={true}
+            row={false}
+            swatchesLast={true}
+            swatches={true}
+            // discrete={false}
+          />
+          <Pressable
+            style={styles.closeModalButton}
+            onPress={() => {
+              setModalVisible(false);
+            }}
+          >
+            <Ionicons
+              style={{ textAlign: "center" }}
+              name="ios-close"
+              size={24}
+              color="black"
+            />
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const submitColor = () => {
+  if (colorName === "") {
+    Alert.alert("Invalid Color Name", "Please enter a color name...");
+    return;
+  }
+  axios
+    .post("http://localhost:8080/colors", {
+      colorName,
+      color,
+      description,
+    })
+    .then((d) => {
+      setModalVisible(false);
+      setColor("#ffffff");
+      setColorName("");
+      setDescription("");
+      console.log(d);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+export default AddColorModal;
--- ColorCarousel.js
+++ ColorCarousel.js
@@ -1,30 +1,22 @@
 import React, { useState, useRef, useEffect } from "react";
 import {
   Animated,
-  View,
-  Text,
   Pressable,
   Dimensions,
-  Modal,
-  TextInput,
-  Alert,
   SafeAreaView,
   TouchableHighlight,
   Clipboard,
 } from "react-native";
 import styles from "./styles";
 import { AntDesign, Feather, Ionicons } from "react-native-vector-icons";
-import ColorPicker from "react-native-wheel-color-picker";
 import Swatch from "./Swatch";
-import axios from "axios";
+import AddColorModal from "./AddColorModal";
 
 function ColorCarousel({ setWelcome, currentColors }) {
   const scroll = useRef(new Animated.Value(0)).current;
   const [scrollIndex, setScrollIndex] = useState(0);
   const [modalVisible, setModalVisible] = useState(false);
-  const [color, setColor] = useState("#ffffff");
-  const [colorName, setColorName] = useState("");
-  const [description, setDescription] = useState("");
+
   const scrollUpVis = scrollIndex !== 0;
   const scrollDownVis = scrollIndex !== -currentColors.length + 1;
   const [windowWidth, setWindowWidth] = useState(
@@ -50,28 +42,6 @@ function ColorCarousel({ setWelcome, currentColors }) {
     }
   };
 
-  const submitColor = () => {
-    if (colorName === "") {
-      Alert.alert("Invalid Color Name", "Please enter a color name...");
-      return;
-    }
-    axios
-      .post("http://localhost:8080/colors", {
-        colorName,
-        color,
-        description,
-      })
-      .then((d) => {
-        setModalVisible(false);
-        setColor("#ffffff");
-        setColorName("");
-        setDescription("");
-        console.log(d);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
   useEffect(() => {
     Animated.timing(scroll, {
       toValue: windowHeight * scrollIndex,
@@ -169,82 +139,11 @@ function ColorCarousel({ setWelcome, currentColors }) {
           color="black"
         />
       </Pressable>
-      <Modal
-        style={styles.addColorModal}
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View
-            style={{
-              width: "90%",
-              height: "75%",
-              backgroundColor: color,
-              opacity: 1,
-              borderRadius: 10,
-              display: "flex",
-              flexDirection: "column",
 
-              alignItems: "center",
-              shadowColor: "#000",
-              shadowOffset: { width: 0, height: 1 },
-              shadowOpacity: 0.5,
-              shadowRadius: 5,
-            }}
-          >
-            <Text style={styles.colorNameLabel}>Color Name:</Text>
-            <TextInput
-              style={styles.input}
-              value={colorName}
-              onChangeText={(text) => setColorName(text)}
-            />
-            <Text style={styles.descriptionLabel}>Description:</Text>
-            <Text style={styles.descriptionText}>
-              (List of Words Separated By Commas)
-            </Text>
-            <TextInput
-              style={styles.input}
-              value={description}
-              onChangeText={(text) => setDescription(text)}
-            />
-            <Pressable style={styles.submitButton} onPress={submitColor}>
-              <Text>Submit</Text>
-            </Pressable>
-            <Text>{color}</Text>
-            <ColorPicker
-              style={styles.picker}
-              color={color}
-              onColorChange={(c) => setColor(c)}
-              // onColorChangeComplete={this.onColorChangeComplete}
-              thumbSize={30}
-              sliderSize={20}
-              noSnap={true}
-              row={false}
-              swatchesLast={true}
-              swatches={true}
-              // discrete={false}
-            />
-            <Pressable
-              style={styles.closeModalButton}
-              onPress={() => {
-                setModalVisible(false);
-              }}
-            >
-              <Ionicons
-                style={{ textAlign: "center" }}
-                name="ios-close"
-                size={24}
-                color="black"
-              />
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
+      <AddColorModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
     </SafeAreaView>
   );
 }
